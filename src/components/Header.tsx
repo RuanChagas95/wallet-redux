8fcc7ee5709e8c6a9678e03a03b08dbf95465a9c
Ruan Chagas <ruanchagas95@gmail.com>
2023-08-31 16:10:24 -0300

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,32 @@
+import { useSelector } from 'react-redux';
+
 function Header() {
-  return <div>Header</div>;
+  const email = useSelector((state: any) => state.user.email);
+  const currency = 'BRL';
+  return (
+    <header>
+      <p>
+        Despesas:
+        {' '}
+        <label data-testid="total-field">
+          {0}
+        </label>
+      </p>
+
+      <p>
+        Moeda:
+        {' '}
+        <label data-testid="header-currency-field">{currency}</label>
+      </p>
+
+      <p>
+        User:
+        {' '}
+        <label data-testid="email-field">{email}</label>
+
+      </p>
+    </header>
+  );
 }
 
 export default Header;
